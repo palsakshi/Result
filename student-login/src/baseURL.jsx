f89ca9964f9e651885a96e09685082130a5925c6
sakshi <pal74089@gmail.com>
2025-04-15 10:56:29 +0530

    backend updates

--- student-login/src/baseURL.jsx
+++ student-login/src/baseURL.jsx
@@ -4,9 +4,10 @@
 let baseURL = import.meta.env.VITE_API_BASE_URL;
 
 if(!baseURL){
+    console.log(import.meta.env.MODE);
     baseURL = import.meta.env.MODE === 'development'
     ? 'http://localhost:3000'
-    : 'https://your-live-domain.com';
+    : 'https://result-3.onrender.com';
 }
 console.log("Current base url:", baseURL )
 export default  baseURL;
