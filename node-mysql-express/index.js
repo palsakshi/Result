748390b8c2b931c7f0087c526ebf75caa8b55790
sakshi <pal74089@gmail.com>
2025-04-18 15:40:46 +0530

    added new updates

--- node-mysql-express/index.js
+++ node-mysql-express/index.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT || 5000;
 const isProduction = process.env.NODE_ENV === 'production';
 
 app.use(cors({
-  origin: 'https://resultstudents-git-main-sakshis-projects-fd2c56cf.vercel.app/',
-
-   methods:['POST', 'GET', 'PUT', 'DELETE', 'UPDATE'],
+  origin: 'https://resultstudents-git-main-sakshis-projects-fd2c56cf.vercel.app',
   credentials: true
 }));
 app.use(express.json());
