1a9de38a766a893d3c78f2ef9a5aa6859a287f5f
sakshi <pal74089@gmail.com>
2025-04-21 16:45:43 +0530

    updates  production

--- node-mysql-express/migrations/20250410112339-add-role-and-name-to-userlogin.js
+++ node-mysql-express/migrations/20250410112339-add-role-and-name-to-userlogin.js
@@ -2,19 +2,19 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('userlogins', 'role', {
+    await queryInterface.addColumn('UserLogins', 'role', {
       type: Sequelize.STRING,
       defaultValue: 'admin'
     });
 
-    await queryInterface.addColumn('userlogins', 'name', {
+    await queryInterface.addColumn('UserLogins', 'name', {
       type: Sequelize.STRING,
       defaultValue: 'Admin1'
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('userlogins', 'role');
-    await queryInterface.removeColumn('userlogins', 'name');
+    await queryInterface.removeColumn('UserLogins', 'role');
+    await queryInterface.removeColumn('UserLogins', 'name');
   }
 };
