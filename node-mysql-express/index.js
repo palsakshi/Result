7fe11918b9fbfff00da8ad4df475123fbef55d7c
sakshi <pal74089@gmail.com>
2025-04-21 13:22:06 +0530

    updates  production

--- node-mysql-express/index.js
+++ node-mysql-express/index.js
@@ -8,10 +8,12 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const isProduction = process.env.NODE_ENV === 'production';
 
+// console.log(isProduction);
 const allowedOrigins = isProduction
   ? ['https://myresultapp.vercel.app']
   : ['http://localhost:5173'];
 
+  // console.log(allowedOrigins);
   app.use(cors({
     origin: function (origin, callback) {
       console.log('🌐 Incoming origin:', origin);
